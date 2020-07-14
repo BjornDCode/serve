8f2aed4b826ef54d11085583969115c8289931d4
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-14 17:54:10 +0200

    Add validator functions

---
+++ src/config/validators.js
@@ -0,0 +1,7 @@
+import { colors, shades, fontSizes, spaces, alignments } from '@/config/values'
+
+export const isValidColor = color => colors.includes(color)
+export const isValidShade = color => shades.includes(color)
+export const isValidFontSize = color => fontSizes.includes(color)
+export const isValidSpace = color => spaces.includes(color)
+export const isValidAlignment = color => alignments.includes(color)
---
+++ src/config/values.js
@@ -0,0 +1,64 @@
+export const shades = [
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+]
+
+export const colors = [
+    'transparent',
+    'current',
+    'black',
+    'white',
+    'gray',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'teal',
+    'blue',
+    'indigo',
+    'purple',
+    'pink',
+]
+
+export const fontSizes = [
+    'xs',
+    'sm',
+    'base',
+    'lg',
+    'xl',
+    '2xl',
+    '3xl',
+    '4xl',
+    '5xl',
+    '6xl',
+]
+
+export const spaces = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    8,
+    10,
+    12,
+    16,
+    20,
+    24,
+    32,
+    40,
+    48,
+    56,
+    64,
+]
+
+export const alignments = ['start', 'center', 'end']
