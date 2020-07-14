755e6d9938131814db57830c09a0503f0017f068
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-14 18:25:46 +0200

    Refactor Icon props

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,9 +1,15 @@
 <template>
-    <Zondicon :icon="name" class="fill-current" />
+    <Zondicon :icon="name" class="fill-current" :class="classes" />
 </template>
 
 <script>
     import Zondicon from 'vue-zondicons'
+
+    import {
+        isValidSize,
+        isValidColor,
+        isValidShade,
+    } from '@/config/validators'
 
     export default {
         components: {
@@ -14,6 +20,30 @@
             name: {
                 type: String,
                 required: true,
+            },
+            color: {
+                type: String,
+                default: 'transparent',
+                validator: isValidColor,
+            },
+            shade: {
+                type: String,
+                validator: isValidShade,
+            },
+            size: {
+                type: Number,
+                default: 4,
+                validator: isValidSize,
+            },
+        },
+
+        computed: {
+            classes() {
+                return [
+                    `text-${this.color}${this.shade ? `-${this.shade}` : ''}`,
+                    `h-${this.size}`,
+                    `w-${this.size}`,
+                ]
             },
         },
     }
