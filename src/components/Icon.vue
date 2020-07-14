778a5c5d69aa672a029d34e9534a6cc1ac44fa51
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-14 16:56:08 +0200

    Make Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+    <Zondicon :icon="name" />
+</template>
+
+<script>
+    import Zondicon from 'vue-zondicons'
+
+    export default {
+        components: {
+            Zondicon,
+        },
+
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+        },
+    }
+</script>
