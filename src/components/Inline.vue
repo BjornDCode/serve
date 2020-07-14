f890e8a49d0d341e0172ebf1334e3349c0fa758a
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-14 17:55:05 +0200

    Add Inline primitive

---
+++ src/components/Inline.vue
@@ -0,0 +1,34 @@
+<template>
+    <Component :is="component" class="flex" :class="classes" v-bind="$attrs">
+        <slot />
+    </Component>
+</template>
+
+<script>
+    import { isValidAlignment, isValidSpace } from '@/config/validators'
+
+    export default {
+        props: {
+            component: {
+                type: String,
+                default: 'div',
+            },
+            align: {
+                type: String,
+                default: 'center',
+                validator: isValidAlignment,
+            },
+            space: {
+                type: Number,
+                default: 0,
+                validator: isValidSpace,
+            },
+        },
+
+        computed: {
+            classes() {
+                return [`items-${this.align}`, `space-x-${this.space}`]
+            },
+        },
+    }
+</script>
