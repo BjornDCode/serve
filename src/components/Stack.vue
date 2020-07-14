802317d5fad2884d1c4e98d8d0e8b3daa91a54e9
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-14 17:55:17 +0200

    Add Stack primitive

---
+++ src/components/Stack.vue
@@ -0,0 +1,39 @@
+<template>
+    <Component
+        :is="component"
+        class="flex flex-col"
+        :class="classes"
+        v-bind="$attrs"
+    >
+        <slot />
+    </Component>
+</template>
+
+<script>
+    import { isValidAlignment, isValidSpace } from '@/config/validators'
+
+    export default {
+        props: {
+            component: {
+                type: String,
+                default: 'div',
+            },
+            align: {
+                type: String,
+                default: 'start',
+                validator: isValidAlignment,
+            },
+            space: {
+                type: Number,
+                default: 0,
+                validator: isValidSpace,
+            },
+        },
+
+        computed: {
+            classes() {
+                return [`items-${this.align}`, `space-y-${this.space}`]
+            },
+        },
+    }
+</script>
