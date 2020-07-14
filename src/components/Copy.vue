119276893e1c46d3ae58087a3f2cc61219032462
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-14 17:54:39 +0200

    Add Text primitive

---
+++ src/components/Copy.vue
@@ -0,0 +1,45 @@
+<template>
+    <Component :is="component" :class="classes">
+        <slot />
+    </Component>
+</template>
+
+<script>
+    import {
+        isValidFontSize,
+        isValidColor,
+        isValidShade,
+    } from '@/config/validators'
+
+    export default {
+        props: {
+            component: {
+                type: String,
+                default: 'span',
+            },
+            size: {
+                type: String,
+                default: 'base',
+                validator: isValidFontSize,
+            },
+            color: {
+                type: String,
+                default: 'black',
+                validator: isValidColor,
+            },
+            shade: {
+                type: String,
+                validator: isValidShade,
+            },
+        },
+
+        computed: {
+            classes() {
+                return [
+                    `text-${this.size}`,
+                    `text-${this.color}${this.shade ? `-${this.shade}` : ''}`,
+                ]
+            },
+        },
+    }
+</script>
