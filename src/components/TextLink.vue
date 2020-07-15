d985adfed54408fc20a5bb9b05aca3791fa68550
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-07-15 15:29:24 +0200

    Create TextLink

---
+++ src/components/TextLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <Inline
+        component="RouterLink"
+        v-bind="$attrs"
+        class="focus:outline-none focus:shadow-outline"
+    >
+        <slot />
+    </Inline>
+</template>
+
+<script>
+    import Inline from '@/components/Inline'
+
+    export default {
+        components: {
+            Inline,
+        },
+    }
+</script>
