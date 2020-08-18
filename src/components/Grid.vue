71f900001bc0777ce8a275db53da87a220029a9f
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-08-18 18:43:07 +0200

    Create Grid components

---
+++ src/components/Grid.vue
@@ -0,0 +1,32 @@
+<template>
+    <Box class="grid" :class="classes" v-bind="$attrs">
+        <slot />
+    </Box>
+</template>
+
+<script>
+    import Box from '@/components/Box'
+
+    export default {
+        components: {
+            Box,
+        },
+
+        props: {
+            columns: {
+                type: Number,
+                required: true,
+            },
+            gap: {
+                type: Number,
+                default: 0,
+            },
+        },
+
+        computed: {
+            classes() {
+                return [`grid-cols-${this.columns}`, `gap-${this.gap}`]
+            },
+        },
+    }
+</script>
---
+++ src/components/GridItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="classes">
+        <slot />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            span: {
+                type: Number,
+                default: 1,
+            },
+        },
+
+        computed: {
+            classes() {
+                return [`col-span-${this.span}`]
+            },
+        },
+    }
+</script>
