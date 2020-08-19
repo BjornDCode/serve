f3094b5ac7cb015a23e7d54846062d528cb83d8d
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-08-19 09:28:39 +0200

    Create Table components

---
+++ src/components/Table.vue
@@ -0,0 +1,51 @@
+<template>
+    <table class="border-separate">
+        <slot />
+    </table>
+</template>
+
+<style>
+    table {
+        border-spacing: 0px;
+    }
+
+    table tr:nth-child(2n) td {
+        @apply bg-gray-100;
+    }
+
+    table td {
+        @apply border-gray-200;
+    }
+
+    table tr:first-child td {
+        @apply border-t;
+    }
+
+    table tr:last-child td {
+        @apply border-b;
+    }
+
+    table tr td:first-child {
+        @apply border-l;
+    }
+
+    table tr td:last-child {
+        @apply border-r;
+    }
+
+    table tr:first-child td:first-child {
+        @apply rounded-tl;
+    }
+
+    table tr:first-child td:last-child {
+        @apply rounded-tr;
+    }
+
+    table tr:last-child td:first-child {
+        @apply rounded-bl;
+    }
+
+    table tr:last-child td:last-child {
+        @apply rounded-br;
+    }
+</style>
