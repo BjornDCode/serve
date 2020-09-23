3a3371479731888f2a962ac0852016bcd32475cc
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-09-23 14:16:08 +0200

    Build config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [],
     theme: {
         extend: {
             fontFamily: {
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,20 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             preload: 'src/preload.js',
+            mainProcessWatch: [
+                'src/background.js',
+                'src/commands.js',
+                'src/commands.js',
+                'src/exceptions/*.js',
+                'src/shell/*.js',
+            ],
+            builderOptions: {
+                appId: 'com.serve.serve',
+                productName: 'Serve',
+                mac: {
+                    category: 'public.app-category.developer-tools',
+                },
+            },
         },
     },
 }
