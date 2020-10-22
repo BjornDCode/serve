bed02cc54adb46df88407a57ff63c01b04127b30
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-10-22 15:59:30 +0200

    Add extra props to menu item

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -3,9 +3,19 @@
         <Submenu label="Tools">
             <MenuItem role="reload">Reload</MenuItem>
         </Submenu>
-        <Submenu label="Developer" :platforms="['windows', 'linux']">
-            <MenuItem role="toggleDevTools" :platforms="['windows']">
+        <Submenu label="Developer">
+            <MenuItem role="toggleDevTools">
                 Toggle Dev Tools
+            </MenuItem>
+            <MenuItem
+                :enabled="true"
+                sublabel="Test"
+                toolTip="This is a tooltip"
+                accelerator="CmdOrCtrl+Shift+p"
+                type="checkbox"
+                :checked="true"
+            >
+                Yolololo
             </MenuItem>
         </Submenu>
         <Submenu role="windowMenu" />
--- src/menu/MenuItem.vue
+++ src/menu/MenuItem.vue
@@ -5,7 +5,73 @@
 
     export default {
         props: {
-            role: String,
+            role: {
+                type: String,
+                validator: role =>
+                    [
+                        'undo',
+                        'redo',
+                        'cut',
+                        'copy',
+                        'paste',
+                        'pasteAndMatchStyle',
+                        'delete',
+                        'selectAll',
+                        'reload',
+                        'forceReload',
+                        'toggleDevTools',
+                        'resetZoom',
+                        'zoomIn',
+                        'zoomOut',
+                        'togglefullscreen',
+                        'window',
+                        'minimize',
+                        'close',
+                        'help',
+                        'about',
+                        'services',
+                        'hide',
+                        'hideOthers',
+                        'unhide',
+                        'quit',
+                        'startSpeaking',
+                        'stopSpeaking',
+                        'zoom',
+                        'front',
+                        'appMenu',
+                        'fileMenu',
+                        'editMenu',
+                        'viewMenu',
+                        'recentDocuments',
+                        'toggleTabBar',
+                        'selectNextTab',
+                        'selectPreviousTab',
+                        'mergeAllWindows',
+                        'clearRecentDocuments',
+                        'moveTabToNewWindow',
+                        'windowMenu',
+                    ].includes(role),
+            },
+            type: {
+                type: String,
+                default: 'normal',
+            },
+            label: String,
+            sublabel: String,
+            toolTip: String,
+            accelerator: String,
+            visible: {
+                type: Boolean,
+                default: true,
+            },
+            enabled: {
+                type: Boolean,
+                default: true,
+            },
+            checked: {
+                type: Boolean,
+                default: false,
+            },
             platforms: {
                 type: Array,
                 default: () => ['linux', 'mac', 'windows'],
@@ -22,7 +88,15 @@
             template() {
                 return {
                     id: this.id,
+                    click: this.click,
                     role: this.role,
+                    type: this.type,
+                    sublabel: this.sublabel,
+                    toolTip: this.toolTip,
+                    accelerator: this.accelerator,
+                    visible: this.visible,
+                    enabled: this.enabled,
+                    checked: this.checked,
                 }
             },
         },
@@ -37,13 +111,17 @@
                     }
 
                     EventBus.$emit('update-menuitem', {
-                        ...this.template,
+                        ...JSON.parse(JSON.stringify(this.template)),
                         label:
                             this.$scopedSlots.default()[0].text ||
                             this.template.label,
                         parentId: this.$parent.template.id,
                     })
                 })
+            },
+
+            click(menuItem, browserWindow, event) {
+                this.$emit('click', { menuItem, browserWindow, event })
             },
         },
 
